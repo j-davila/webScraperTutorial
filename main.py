aa9b5576fdcd80a8db1b1476a3d71c703d9f2fb0
José L Dávila Montalvo <34734577+j-davila@users.noreply.github.com>
2021-04-03 22:43:20 -0700

    finished tutorial Final code with all 105 product listings and information successfully scraped.

--- main.py
+++ main.py
@@ -10,28 +10,31 @@ import pandas as pd
 
 
 #setting base,headers, variables, and lists
-baseUrl = "https://www.thewhiskyexchange.com"
+#baseUrl = "https://www.thewhiskyexchange.com"
+baseUrl = "https://books.toscrape.com/"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ''AppleWebKit''/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
 productlinks = []
 data=[]
-
+c = 0
 
 #HTTP call to extract li HTML element(item in a list)
-k = requests.get('https://www.thewhiskyexchange.com/c/35/japanese-whisky').text
-soup=BeautifulSoup(k, 'html.parser')
-productlist = soup.find_all("li",{"class":"product-grid__item"})
-c = 0
-#print(productlist)
+#k = requests.get('https://www.thewhiskyexchange.com/c/35/japanese-whisky').text
+k = requests.get('https://books.toscrape.com/index.html').text
+soup=BeautifulSoup(k,'html.parser')
+#productlist = soup.find_all("li", {"class":"product-grid__item"})
+productlist = soup.find_all("li",
+                            {"class":"col-xs-6 col-sm-4 col-md-3 col-lg-3"})
+print(productlist)
 
 for x in range(1, 6):
  k = requests.get(
   'https://www.thewhiskyexchange.com/c/35/japanese-whisky?pg=''{}&psize=24&sort=pasc'.format(x)).text
 
  soup = BeautifulSoup(k, 'html.parser')
- productlist = soup.find_all("li", {"class": "product-grid__item"})
+ productlist = soup.find_all("li",{"class": "product-grid__item"})
 
  for product in productlist:
-  link = product.find("a", {"class": "product-card"}).get('href')
+  link = product.find("a",{"class": "product-card"}).get('href')
   productlinks.append(baseUrl + link)
 
 for link in productlinks:
